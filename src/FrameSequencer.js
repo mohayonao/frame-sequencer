786e0f72a0f01896f1c25a4b37db17bc2ab174a7
mohayonao <mohayonao@gmail.com>
2015-08-26 13:53:19 +0900

    add FrameSequencer

---
+++ src/FrameSequencer.js
@@ -0,0 +1,74 @@
+import EventEmitter from "@mohayonao/event-emitter";
+import defaults from "@mohayonao/defaults";
+import appendIfNotExists from "@mohayonao/append-if-not-exists";
+import removeIfExists from "@mohayonao/remove-if-exists";
+
+export default class FrameSequencer extends EventEmitter {
+  constructor(opts = {}) {
+    super();
+
+    this.interval = defaults(opts.interval, 1);
+    this.elements = [];
+    this.actives = [];
+  }
+
+  add(element) {
+    return appendIfNotExists(this.elements, element);
+  }
+
+  remove(element) {
+    removeIfExists(this.actives, element);
+    return removeIfExists(this.elements, element);
+  }
+
+  removeAll() {
+    this.actives.splice(0);
+    this.elements.splice(0);
+  }
+
+  update(t0, t1) {
+    let actives = this.actives;
+
+    this.onResume(t0, t1);
+
+    let data = [];
+    let elements = this.elements.sort((a, b) => a.layer - b.layer);
+
+    for (let i = 0; i < elements.length; i++) {
+      let element = elements[i];
+
+      if (t0 <= element.stopTime && element.startTime < t1) {
+        if (appendIfNotExists(actives, element)) {
+          element.init(t0, t1);
+          this.onStartElement(element);
+        }
+
+        let result = element.update(t0, t1);
+
+        if (Array.isArray(result)) {
+          data.push(...result);
+        }
+      }
+
+      if (element.stopTime < t1) {
+        if (removeIfExists(actives, element)) {
+          element.dispose(t0, t1);
+          this.onStopElement(element);
+        }
+      }
+    }
+
+    this.onPause(t0, t1);
+
+    this.elements = elements.filter(element => t1 <= element.stopTime);
+
+    if (data.length) {
+      this.emit("data", data);
+    }
+  }
+
+  onResume() {}
+  onStartElement() {}
+  onStopElement() {}
+  onPause() {}
+}
